0dc5748391e3d65753ca4d82a2ebd9179c50e971
tariq7299 <tr.sar77an78@gmail.com>
2024-05-16 13:01:50 +0300

    - Styled the icon buttons a little bit more - Styled the textarea of thje editing a fact in the table

--- CatFactsClient/src/components/UsersCatFactsTable/UsersCatFactsTable.jsx
+++ CatFactsClient/src/components/UsersCatFactsTable/UsersCatFactsTable.jsx
@@ -8,6 +8,12 @@ import { useAuth } from '../../hooks/AuthProvider';
 import { useAlert } from '../../hooks/AlertProvider';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import ExitIcon from '../common/Icons/ExitIcon';
+import PencilIcon from '../common/Icons/PencilIcon';
+import TrashIcon from '../common/Icons/TrashIcon';
+import SaveIcon from '../common/Icons/SaveIcon';
+
+
 
 const UsersCatFactsTable = ({ newFactAdded, setNewFactAdded }) => {
 
@@ -214,20 +220,11 @@ const UsersCatFactsTable = ({ newFactAdded, setNewFactAdded }) => {
         <div className="icon-button-container custom-font">
           {row.owner.toLowerCase() === username ? (
             <><button onClick={() => handleDeletUserCatFact(row.factId)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
-  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-</svg>
+           <TrashIcon/>
           </button>
           <button onClick={() => handleEditCatFact(row.factId)}>
-          {row.editMode ? <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
-  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-</svg> : <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
-  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-</svg>}
-        </button> {row.editMode && <button  onClick={handleSubmit((data) =>onSubmit(data, row.factId))}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-floppy2-fill" viewBox="0 0 16 16">
-  <path d="M12 2h-2v3h2z"/>
-  <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5v13A1.5 1.5 0 0 0 1.5 16h13a1.5 1.5 0 0 0 1.5-1.5V2.914a1.5 1.5 0 0 0-.44-1.06L14.147.439A1.5 1.5 0 0 0 13.086 0zM4 6a1 1 0 0 1-1-1V1h10v4a1 1 0 0 1-1 1zM3 9h10a1 1 0 0 1 1 1v5H2v-5a1 1 0 0 1 1-1"/>
-</svg></button>}</>
+          {row.editMode ? <ExitIcon/> : <PencilIcon/>}
+        </button> {row.editMode && <button  onClick={handleSubmit((data) =>onSubmit(data, row.factId))}><SaveIcon/></button>}</>
             
           ) : (
             ''
